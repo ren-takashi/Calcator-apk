c6f3b891fbc41dc92a3edbc32b352dedc2b246e3
ren-takashi <hshsgd845@gmail.com>
2026-08-15 19:14:51 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.calculator"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.calculator"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("net.objecthunter:exp4j:0.4.8") 
+}
